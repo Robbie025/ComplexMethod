989672365458bd813d2ec921693662d6679d5abf
Varun Gopinath <varungopi@gmail.com>
2014-12-24 01:47:34 +0100

    Updated the documentation.

--- python/objfunc2.py
+++ python/objfunc2.py
@@ -1,16 +1,23 @@
 import numpy as np
-# Step function also known as the dejong function _
 def install(a):
-        #n=np.size(a)
-        #objval=0;
-        # a much better and elegant solution would be to use sum(sum(np.floor(a)))
-        objval=sum(sum(np.floor(a)))
-        #for i in range(0,n):
-        #    objval = objval + np.floor(a.item(i));
-        
+        objval=np.sum(np.sum(np.floor(a)))
         return objval
  
 if __name__ == "__main__":
+ """Implements the  Step function and is also known as DE JONG's Function 3
+  
+ 
+ Syntax:  ObjVal = dejong3(x)
+ Input parameters:  x - A numpy array of real values
+ eg:  x = np.array([[1.2,2.2,-1.3]])
+
+ Output parameters:
+    objval    - One real value according to DeJong's function 3.
+ 
+ Usage: python objfunc2.py               
+ """
+ 
  x=np.array([[1.2,2.2,-1.3]])
- print install(x)," should be two for [[1.2,1.2]] and three for  [[1.2,1.2,1.3]] and one  for [[1.2,2.2,-1.2]] " 
+ print "x = ",x, " and the function value is ", install(x)
+ print " Should be two for [[1.2,1.2]] and three for  [[1.2,1.2,1.3]] and one  for [[1.2,2.2,-1.2]] " 
 
