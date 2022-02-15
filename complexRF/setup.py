8d2597c8b0e631796a4b8aa5314c3afea1b4bcc9
Robbie025 <varungopi@gmail.com>
2022-02-15 18:02:54 +0100

    Updated setup with changes to make it look nice as print out nice resutls

--- complexRF/setup.py
+++ complexRF/setup.py
@@ -1,45 +1,31 @@
 import argparse
 import concurrent.futures
-from doctest import DocTestFailure
 import importlib
 import sys
 import time as t
-from locale import strcoll
 
 import numpy as np
 
 import src.complexpy as cp  # Implementation of complexpy
 
-# USAGE:
+# USAGE: from terminal window
 # python3 setup.py --xup "512 512" --xlow '-512 -511'
-# python3 setup.py --xup "512 512" --xlow '-512 -511' --objf 'src.testfunctions.objfunc5' --simple 'False'
+# python3 setup.py --xup "512 512" --xlow '-512 -511' --objf 'src.testfunctions.objfunc5' -p
 
 
 
+def printouttoterminal(i,xmin,fmin,Iterations,noofevaluations,conv):
+    if i == 1:
+        print("{:12}".format("No."), '{:16}'.format("xmin"), "{:8}".format("fmin"), "{:10}".format("Iterations"), "{:7}".format("Evals"), "{:4}".format("conv") )         
 
-def linearprocesscomplexserarch():
-    return 0
-
-def multiprocesscomplexserarch():
-    return 0
-
-def printresults():
-    if n==1:
-        print("Only the table header")
-    if args.detailed == True:
-        print('Current result')
-    else:
-            print('skik this')
-    return 0
+    print("{:2}".format(i),'{:>17}'.format(np.array2string(xmin)), \
+         "{:>12}".format(np.array2string(fmin)),"{:8}".format(Iterations), "{:10}".format(noofevaluations), "{:4}".format(conv) )
 
 
 
 if __name__ == "__main__":
 
-    """
-    User can edit these default values when runing this file in a python interpretor. 
-    TO DO: the if else statment to run either a default_process as well as dagult_simple
-    """
+    """ User can edit these default values when runing this file in a python interpretor. """
     # Default values for lower bound e.g. '-512.0 -512.0'
     default_xlow = '-5.0 -5.0' 
 
@@ -51,28 +37,26 @@ if __name__ == "__main__":
     default_samplingmethod_option = False # True for shuffling the initial sample. Does not really help?
 
     # relative to path of this file. Objective function definitions. more work to be done. see src/testfunctions. 
-    default_objfunc = "src.testfunctions.objfunc5" 
+    default_objfunc = "src.testfunctions.objfunc" 
     
     # n - How many runs do you want to run?
     default_n = 100 
-    
-    
-    # ************************************************
-    """ The arguments that can used to run setup.py """
 
-    # This is for arguments that are passed in through a Terminal. Is this really useful?
-    parser = argparse.ArgumentParser(description='test function')
+    
+    """ The arguments that can used to run setup.py and passed through a terminal"""
+    parser = argparse.ArgumentParser(description='Setup file for complexpy. Run this either from a commanf line wiht arguments \
+        or simply python3 setup.py')
     parser.add_argument('-l','--xlow', \
         type=str, \
             required=False, \
                 default = default_xlow, \
-                    help='Lower bound of the test function. This is a List; e.g.: [ -15, -15] ')
+                    help='Lower bound of the test function. This is a List; e.g.:' +'-5.0 -5.0')
 
     parser.add_argument('-u','--xup', \
         type=str, \
             required=False, \
                 default= default_xup, \
-                    help='Upper bound of the test function. This is a List; e.g.: [15, 15] ')
+                    help='Upper bound of the test function. This is a List; e.g.:'+'5.0 5.0' )
 
     parser.add_argument('-n',\
         required=False,\
@@ -83,9 +67,9 @@ if __name__ == "__main__":
     parser.add_argument('--sample', \
         choices = ['uniform','LHS','debug'], \
             type = str, \
-                                required=False,\
-            default = default_samplingmethod, \
-                help='--sample -lhc or --sample -uniform will tell complexpy which start vector to use.')
+                required=False,\
+                    default = default_samplingmethod, \
+                        help='--sample -lhc or --sample -uniform will tell complexpy which start vector to use.')
 
     parser.add_argument('-o',"--objf", \
         type = str, \
@@ -106,8 +90,8 @@ if __name__ == "__main__":
                     default=False)
 
     args = parser.parse_args()
-    # ************************************************
-   
+
+
     #Getting the objective function handler from the arguments
     arg_function = args.objf    
     objfuncHandle = importlib.import_module(arg_function, ".")
@@ -121,68 +105,63 @@ if __name__ == "__main__":
         xlow[i], xup[i] = smallx[i], largex[i]
  
 
-    """
-    Variables to prepare for printout
-    c = number of times complexpy has converged
-    xprint = space for printing xmin
-    """
-    c=0
-    np.set_printoptions(precision=4)
+    i, c = 0, 0 # i is counter for number of runs; c is the number of converged runs
     startime = t.perf_counter()
 
     samplingmethod = args.sample
-    print('first',samplingmethod)
     option=False
 
- 
-    if (not args.process):
-        # Printing out the results
-        print("{:12}".format("No."), '{:16}'.format("xmin"), "{:8}".format("fmin"), "{:10}".format("Iterations"), "{:7}".format("Evals"), "{:4}".format("conv") ) 
-        
-        for i in range(args.n):
+    
+    """The sequential call to complexrf"""
+    if (not args.process):          
+        i=0
+        for i in range(1,args.n+1):
             xmin,fmin,funcVector,allf,Iterations,conv,noofevaluations=  cp.complexpy_(funcname,xlow,xup,samplingmethod,option)
-            if (args.detailed==False and  ( (i+1) == args.n)):
-                    print("{:2}".format(i+1),"/",args.n,'{:>17}'.format(np.array2string(xmin)), "{:>12}".format(np.array2string(fmin)),"{:8}".format(Iterations), "{:10}".format(noofevaluations), "{:4}".format(conv) ) #,"\t","{:7.3f}".format(time.time()-start_))
-            
-            if (args.detailed == True):
-                print("{:2}".format(i+1),'{:>17}'.format(np.array2string(xmin)), "{:>12}".format(np.array2string(fmin)),"{:8}".format(Iterations), "{:10}".format(noofevaluations), "{:4}".format(conv) ) #,"\t","{:7.3f}".format(time.time()-start_))
+
+            if (args.detailed == True): # Print all values only for -d flag
+                
+
+                printouttoterminal(i,xmin,fmin,Iterations,noofevaluations,conv)
                
             if conv>1:
                 c=c+1
+                if i==1:
+                    f_best, x_best, i_best = fmin, xmin, i
+                elif fmin < f_best:
+                    f_best, x_best, i_best = fmin, xmin, i          
 
-    print("Number of times that it has converged c= %s out of %s"%(c,i+1))
-    endtime = t.perf_counter()
-    print(f'Total time taken is  {round(endtime-startime,2)}') # works only in python> 3.x
-    
-
+    """The Concurrent call to complexrf"""
     if args.process:
-        print("ahshit")
         with concurrent.futures.ProcessPoolExecutor() as executor:
             arg=[funcname,xlow,xup,samplingmethod,option]
-            e1 = [executor.submit(cp.complexpy_,*arg) for _ in range(args.n)]
+            e1 = [executor.submit(cp.complexpy_,*arg) for _ in range(args.n)]  
+
+        for f in concurrent.futures.as_completed(e1):
+            resultf = f.result()
+            xmin,fmin,funcvector,allf,Iterations,conv,noofevaluations = resultf[0],resultf[1],resultf[2],resultf[3],resultf[4] ,resultf[5],resultf[6]
+            i=i+1
           
-            i, c = 0, 0 # i is counter for number of runs; c is the number of converged runs
-            np.set_printoptions(precision=3)
-            print("{:12}".format("No."), '{:15}'.format("xmin"), "{:8}".format("fmin"), "{:12}".format("Iterations"), "{:7}".format("Evals"), "{:4}".format("conv") ) 
-            
-            for f in concurrent.futures.as_completed(e1):
-                resultf = f.result()
+            if conv>1:
+                c=c+1
+                if i==1:
+                        f_best, x_best, i_best = fmin, xmin, i
+                elif fmin < f_best:
+                        f_best, x_best, i_best = fmin, xmin, i 
                 
-                xmin,fmin,funcvector, allf, Iterations,conv,noofevaluations = resultf[0],resultf[1],resultf[2],resultf[3],resultf[4] ,resultf[5],resultf[6]
-                if conv>1:
-                    c=c+1
+            if (args.detailed == True):
+                    printouttoterminal(i,xmin,fmin,Iterations,noofevaluations,conv)
+                            
 
-                if (args.detailed==False and  ( (i+1) == args.n)):
-                    print("{:2}".format(i+1),"/",args.n,'{:>17}'.format(np.array2string(xmin)), "{:>12}".format(np.array2string(fmin)),"{:8}".format(Iterations), "{:10}".format(noofevaluations), "{:4}".format(conv) ) #,"\t","{:7.3f}".format(time.time()-start_))
-            
-                if (args.detailed == True):
-                    print("{:2}".format(i+1),'{:>17}'.format(np.array2string(xmin)), "{:>12}".format(np.array2string(fmin)),"{:8}".format(Iterations), "{:10}".format(noofevaluations), "{:4}".format(conv) ) #,"\t","{:7.3f}".format(time.time()-start_))
-                i=i+1
-            
-        print("Number of times that it has converged c= %s out of %s"%(c,i+1))
-        endtime = t.perf_counter()
-        print(f'Total time taken is  {round(endtime-startime,2)}') # works only in python> 3.x
-               
-
-     
- 
+    print("\nResult Summary")
+    print("1. Objective function:",arg_function)
+    print("2. x* = ",x_best)
+    print("3. f* = ",f_best)
+    print("4. x* and f* found at run",i_best,"/",i)
+    print("5. Convergence rate c= %s / %s"%(c,i))
+    
+    print("\n *********** Other details")
+    if (not args.process):
+        print("The complexRF runs wewre run sequentially. Try --process to make it slightly faster")
+    else: 
+        print("Process was run multithreaded mode. ")
+    print(f'Total time taken is  {round(t.perf_counter()-startime,2)}') # works only in python> 3.x
